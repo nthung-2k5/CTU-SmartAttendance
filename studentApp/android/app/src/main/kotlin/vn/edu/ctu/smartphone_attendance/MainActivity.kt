1da2f7290bdddab004434720890d337805a47378
hintran_05 <you@example.com>
2026-07-01 12:17:34 +0700

    initial commit

---
+++ studentApp/android/app/src/main/kotlin/vn/edu/ctu/smartphone_attendance/MainActivity.kt
@@ -0,0 +1,5 @@
+package vn.edu.ctu.smartphone_attendance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ studentApp/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CTU Điểm Danh',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // Cau hinh mau chu de
+        primarySwatch: const MaterialColor(0xFF003DA5, <int, Color>{
+          50: Color(0xFFE0E8F5),
+          100: Color(0xFFB3C5E6),
+          200: Color(0xFF809ED5),
+          300: Color(0xFF4D77C4),
+          400: Color(0xFF265AB7),
+          500: Color(0xFF003DA5),
+          600: Color(0xFF00379D),
+          700: Color(0xFF002F93),
+          800: Color(0xFF00278A),
+          900: Color(0xFF001A79),
+        }),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF003DA5),
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+        fontFamily: 'Roboto',
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
+    );
+  }
+}
